92a37fe21646ab0bf223cbff0e6afcdb0c5c0aee
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-11-01 23:58:41 +0100

    Get paths for unwatched movies and episodes.

--- un-sync.py
+++ un-sync.py
@@ -6,3 +6,14 @@ plex_url = os.getenv('PLEX_URL')
 plex_token = os.getenv('PLEX_TOKEN')
 
 plex = PlexServer(plex_url, plex_token)
+
+for section in plex.library.sections():
+  if section.type == 'movie':
+    for m in section.searchMovies(unwatched=True):
+      print(m.locations)
+
+  if section.type == 'show':
+    seasons = section.searchSeasons(unwatched=True)
+    for s in seasons:
+      for e in s.episodes():
+        print(e.locations)
