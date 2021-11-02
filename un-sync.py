cac852e132c71b0b63885b3e8522ed383c195df8
Markus Reiter <me@reitermark.us>
2021-11-02 17:07:25 +0100

    Compare directories instead of only checking existence.

--- un-sync.py
+++ un-sync.py
@@ -1,3 +1,4 @@
+import filecmp
 import os
 from pathlib import Path
 
@@ -15,6 +16,9 @@ watched_replicas = 1
 WATCHED_REPLICAS = int(os.getenv('WATCHED_REPLICAS', '1'))
 UNWATCHED_REPLICAS = int(os.getenv('UNWATCHED_REPLICAS', '2'))
 LOCATION_PREFIX = Path(os.getenv('LOCATION_PREFIX', ''))
+
+SOURCE_DIR = Path(os.getenv('SOURCE_DIR'))
+REPLICA_DIRS = [Path(dir) for dir in os.getenv('REPLICA_DIRS').split(':')]
 
 # Get a dictionary containing all movie and season paths and their watched status.
 def plex_paths(plex):
@@ -37,46 +41,63 @@ def plex_paths(plex):
 
   return paths
 
-# Ensure `count` replicas of `file_path` exist across all `replica_root_paths`.
-def ensure_replicas(replica_root_paths, file_path, count):
-  replica_root_paths_with_file = []
-  replica_root_paths_without_file = []
+# Ensure `count` replicas of `file_path` exist across all `replica_dirs`.
+def ensure_replicas(source_dir, replica_dirs, file_path, count):
+  replica_dirs_up_to_date = []
+  replica_dirs_outdated = []
   current_count = 0
 
-  for replica_root_path in replica_root_paths:
-    if replica_root_path.join(file_path).exists():
-      replica_root_paths_with_file.push(replica_root_path)
+  for replica_dir in replica_dirs:
+    needs_update = False
+
+    if not (replica_dir/file_path).exists():
+      needs_update = True
+    else:
+      diff = filecmp.dircmp(source_dir/file_path, replica_dir/file_path)
+
+      if diff.left_only or diff.right_only or diff.diff_files:
+        needs_update = True
+
+    if needs_update:
+      replica_dirs_outdated.append(replica_dir)
+    else:
+      replica_dirs_up_to_date.append(replica_dir)
       current_count += 1
 
       # File already has the required number of replicas.
       if current_count >= count:
         return
-    else:
-      replica_root_paths_without_file.push(replica_root_path)
 
   # Skip missing files.
   if current_count == 0:
     return
 
-  replica_root_paths_without_file.sort(key=lambda path: psutil.disk_usage(path).free, reverse=True)
+  replica_dirs_outdated.sort(key=lambda path: psutil.disk_usage(path).free, reverse=True)
 
   additional_needed_replicas = count - current_count
-  src = replica_root_paths_with_file[0]/file_path
-  for replica_root_path_without_file in replica_root_paths_without_file[..additional_needed_replicas]:
-    dst = replica_root_path_without_file/file_path
+  src = replica_dirs_up_to_date[0]/file_path
+  for replica_dir_without_file in replica_dirs_outdated[:additional_needed_replicas]:
+    dst = replica_dir_without_file/file_path
 
-    print(f"cp '{src}' '{dst}'")
+    print(f"cp -R '{src}' '{dst}'")
     continue
 
   return
 
-for (path, watched) in plex_paths(plex).items():
-  if path.is_relative_to(LOCATION_PREFIX):
-    relative_path = path.relative_to(LOCATION_PREFIX)
-    print(relative_path, watched)
+def run(source_dir, replica_dirs):
+  for (path, watched) in plex_paths(plex).items():
+    if path.is_relative_to(LOCATION_PREFIX):
+      relative_path = path.relative_to(LOCATION_PREFIX)
+      print(relative_path, watched)
 
-    root_paths = []
-    replica_count = WATCHED_REPLICAS if watched else UNWATCHED_REPLICAS
-    ensure_replicas(root_paths, relative_path, replica_count)
-  else:
-    print(f"Skipping path '{path}'.")
+      if not (source_dir/relative_path).exists():
+        print(f"Skipping path '{path}'; does not exist in root directory.")
+        continue
+
+      replica_count = WATCHED_REPLICAS if watched else UNWATCHED_REPLICAS
+      ensure_replicas(source_dir, replica_dirs, relative_path, replica_count)
+    else:
+      print(f"Skipping path '{path}'; not relative to `LOCATION_PREFIX`.")
+
+if __name__ == '__main__':
+  run(SOURCE_DIR, REPLICA_DIRS)
